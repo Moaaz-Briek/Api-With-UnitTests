148d1b61c94c78f308d5c0e6e37c1245f8848a4c
moaaz97 <moaaz.briek97@gmail.com>
2023-07-18 00:52:05 +0300

    endPoint to create new travels

---
+++ app/Http/Requests/TravelRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TravelRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'is_public' => 'boolean',
+            'name' => ['required', 'unique:travels'],
+            'description' => ['required'],
+            'number_of_days' => ['required', 'integer']
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\Admin;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('travels', [TravelController::class, 'index']);
 Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
+
+Route::prefix('admin')->middleware(['auth:sanctum', 'role:admin'])->group(function (){
+   Route::post('travels', [Admin\TravelController::class, 'store']);
+});
+
+Route::post('login',  LoginController::class);
